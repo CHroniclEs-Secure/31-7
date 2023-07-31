bbb37bc32c740ddc199017f05d5da17dbad3bb9d
CHr0NiclEs-EM <eri.rivas@icloud.com>
2023-07-31 10:12:41 -0400

    test PyQt6 - Ajout des chaines des caractères.

--- testPyQt6.py
+++ testPyQt6.py
@@ -1,5 +1,5 @@
 #Class rbabari - EMRivas
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLabel
 
 # Étape 1: Créer(instancier) 1 objet app: type: QApplication
 # qui provient de la librairie PyQt6
@@ -7,6 +7,12 @@ app = QApplication([])
 
 # Étape 2: Créer un objet fenetre QWidget
 fen = QWidget()
+fen.setGeometry(20, 313, 1320, 200) #location, dimention
+fen.setWindowTitle("Ma premiere Fenetre ^^,")
+
+# Étape 5: ajout du Label
+lbl1 = QLabel(fen)
+lbl1.setText("On reprend avec la Foi en Dieu.")
 
 # Étape 3: Rentrer la fenetre visible
 fen.show()
