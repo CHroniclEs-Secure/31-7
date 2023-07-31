00b8be0049d5277b04cf9f3ae10212f9b2d82b1e
CHr0NiclEs-EM <eri.rivas@icloud.com>
2023-07-31 10:31:32 -0400

    test PyQt6 - Ajout des boutons et leur actions.

--- testPyQt6.py
+++ testPyQt6.py
@@ -1,5 +1,14 @@
 #Class rbabari - EMRivas
+
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLabel
+
+# Étape 7: On definit l'action des boutons.
+def action1():
+    print("On continue.")
+
+def action2():
+    print("On recomence.")
+
 
 # Étape 1: Créer(instancier) 1 objet app: type: QApplication
 # qui provient de la librairie PyQt6
@@ -21,6 +30,12 @@ lbl1.setText("On reprend avec la Foi en Dieu."
 btn1 = QPushButton(fen)
 btn1.setText("...Continuer...")
 btn1.setGeometry(20, 30, 200, 25)
+btn1.clicked.connect(action1)
+
+btn2 = QPushButton(fen)
+btn2.setText(". Recomencer...")
+btn2.setGeometry(40, 60, 200, 25)
+btn2.clicked.connect(action2)
 
 # Étape 3: Rentrer la fenetre visible
 fen.show()
