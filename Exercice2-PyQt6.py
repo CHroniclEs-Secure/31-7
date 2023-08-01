ffb4542187b573a67911b8c99152bef1ac165399
CHr0NiclEs-EM <eri.rivas@icloud.com>
2023-08-01 09:07:51 -0400

    Exercice 2 - Calculatrice consignes.

--- Exercice2-PyQt6.py
+++ Exercice2-PyQt6.py
@@ -1,4 +1,6 @@
-#Class Babari - EMRivas
-#Creer un calculatrice
-#besoin 11 boutons
-
+# Class Babari - EMRivas
+# Creer un calculatrice
+# besoin 11 boutons
+# -> bouton de 0 à 9
+# -> bouton + et -
+# les boutons doivent être dans une liste
